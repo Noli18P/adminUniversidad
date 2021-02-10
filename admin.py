2c0265c140ff1bff0fabc9c6bfd256e48549c510
Martin Martinez <investingmtz@gmail.com>
2021-02-10 10:16:19 -0600

    Base de datos creada

--- admin.py
+++ admin.py
@@ -2,5 +2,11 @@ import sqlite3
 
 conection = sqlite3.connect('UNIVERSIDAD')
 
+pointer = conection.cursor()
+
+pointer.execute("CREATE TABLE ESTUDIANTES (ID INTEGER PRIMARY KEY AUTOINCREMENT, NOMBRE_ESTUDIANTE VARCHAR(50), EDAD_ESTUDIANTE INTEGER, ESPAÑOL INTEGER, MATEMATICAS INTEGER, CIENCIAS )")
+
+conection.commit()
+
 
 conection.close()
